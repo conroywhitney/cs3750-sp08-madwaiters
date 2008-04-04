944800f62eaa5816245f9af44f5b28d69dca5782
conroy.whitney <conroy.whitney@b52eaba3-024a-0410-8624-7f5465c6ad64>
2008-04-04 18:19:12 +0000

    made so will display correct item name/image (at least for demo items); horizontal display; next -- need to find better way to remove items (currently logout.php clears session)

---
+++ www/sql.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+    mysql_connect( "HOST", "USER", "PASSWORD" ) or
+        die( "Could not connect to database: ".mysql_error() );
+
+    mysql_select_db( "DATABASE" ) or
+	    die( "Could not select database: ".mysql_error() );
+
+?>
---
+++ www/template.php
@@ -0,0 +1,21 @@
+<?php
+
+    function template($row) {
+        $id = $row["id"];
+        $name = $row["name"];
+        $image = $row["image"];
+?>
+        <!-- First Draggable Item -->
+        <div class="cart-items" id="item_<?php echo $id; ?>" style="float: left;">
+            <span><?php echo $name; ?></span><br />
+            <img style="position: relative;" alt="<?php echo $name; ?>" id="item_<?php echo $id; ?>_img" src="images/<?php echo $image; ?>">
+        </div>
+        <script type="text/javascript">
+            //<![CDATA[
+            new Draggable("item_<?php echo $id; ?>", {revert:true})
+            //]]>
+        </script>
+<?php
+    }
+
+?>
