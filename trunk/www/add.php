ae63cc7122b356036d08b62ede663a56a10dbbcb
conroy.whitney <conroy.whitney@b52eaba3-024a-0410-8624-7f5465c6ad64>
2008-04-04 17:40:20 +0000

    added initial drag-drop trials based on script.aculo.us; can drag into favourites (cart) box; will actually try to save that and output correct favourites

---
+++ trunk/www/add.php
@@ -0,0 +1,10 @@
+        <!-- First Draggable Item -->
+        <div class="cart-item" id="product_65_1">
+            <span>The Mushroom-Swiss Burger</span><br />
+            <img style="position: relative;" alt="Mushroom-Swiss" id="product_65_img" src="images/mushroom.gif">
+        </div>
+        <script type="text/javascript">
+            //<![CDATA[
+            new Draggable("product_65_1", {revert:true})
+            //]]>
+        </script>
