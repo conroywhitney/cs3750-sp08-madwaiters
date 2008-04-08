8105e21cdf5eea75d038c3477312cc38102b1cb5
conroy.whitney <conroy.whitney@b52eaba3-024a-0410-8624-7f5465c6ad64>
2008-04-08 05:48:20 +0000

    moved "news" design into header/footer (can see on search page)

---
+++ www/footer.php
@@ -0,0 +1,20 @@
+<!-- End footer -->
+
+</div><!-- End content -->
+
+</div><!-- End page_wrapper -->
+
+<script type="text/javascript">
+//<![CDATA[
+Droppables.add("cart", {accept:'products', hoverclass:'cart-active', onDrop:function(element){new Ajax.Updater('items', 'add.php', {asynchronous:true, evalScripts:true, parameters:'id=' + encodeURIComponent(element.id)})}})
+//]]>
+</script>
+
+<script type="text/javascript">
+//<![CDATA[
+Droppables.add("wastebin", {accept:'cart-items', hoverclass:'wastebin-active', onDrop:function(element){Element.hide(element); new Ajax.Updater('items', 'remove.php', {asynchronous:true, evalScripts:true, parameters:'id=' + encodeURIComponent(element.id)})}})
+//]]>
+</script>
+      
+</body>
+</html>
