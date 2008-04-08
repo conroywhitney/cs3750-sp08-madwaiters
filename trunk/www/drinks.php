e078c9ce285a2d7c76bc804630bc1e3acba3e3c6
conroy.whitney <conroy.whitney@b52eaba3-024a-0410-8624-7f5465c6ad64>
2008-04-08 06:58:26 +0000

    copy-paste add logic to show stuff from db; need to add "splash" page for all who have subsections ...

--- trunk/www/drinks.php
+++ trunk/www/drinks.php
@@ -1,3 +1,32 @@
 <?php require_once("header.php"); ?>
 
+<?php
+
+	$type = $_GET["type"];
+	if ($type) {
+		$query = "SELECT * FROM items WHERE type_id='$type' ";
+		if ($result = mysql_query($query)) {
+			if (mysql_num_rows($result) > 0) {
+			    while ($row = mysql_fetch_assoc($result)) {
+			        // print off its template
+			        echo item_template($row);
+			    }
+			} else {
+				echo "no rows";
+			}
+	    } else {
+			echo "no result";
+		}
+	} else {
+
+?>
+
+	<!-- Show sampling of all -->
+
+<?php
+
+	}
+
+?>
+
 <?php require_once("footer.php"); ?>
--- trunk/www/sides.php
+++ trunk/www/sides.php
@@ -1,3 +1,21 @@
 <?php require_once("header.php"); ?>
 
+<?php
+
+	$query = "SELECT * FROM items WHERE type_id='15' ";
+	if ($result = mysql_query($query)) {
+		if (mysql_num_rows($result) > 0) {
+		    while ($row = mysql_fetch_assoc($result)) {
+		        // print off its template
+		        echo item_template($row);
+		    }
+		} else {
+			echo "no rows";
+		}
+    } else {
+		echo "no result";
+	}
+
+?>
+
 <?php require_once("footer.php"); ?>
