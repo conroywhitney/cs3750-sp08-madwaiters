f311280c9238de0e1114ebf43737f11e879262cb
conroy.whitney <conroy.whitney@b52eaba3-024a-0410-8624-7f5465c6ad64>
2008-04-08 14:54:45 +0000

    added jonathan's drop-down menu

---
+++ trunk/www/dd_menu.php
@@ -0,0 +1,47 @@
+<div id="navcontainer" class="dd_menu">
+	<ul>
+
+
+	<li><a href="entrees.php">ENTREES<!--[if IE 7]><!--></a><!--<![endif]-->
+	<!--[if lte IE 6]><table><tr><td><![endif]-->
+		<ul>
+		<li><a href="entrees.php?type=3">Fire-Grilled Steaks</a></li>
+		<li><a href="entrees.php?type=5">Kickin' Chicken</a></li>
+		<li><a href="entrees.php?type=8">Sizzling Fajitas</a></li>
+		<li><a href="entrees.php?type=9">Savory Seafood</a></li>
+		<li><a href="entrees.php?type=10">Big Mouth Burgers</a></li>
+		<li><a href="entrees.php?type=11">Sandwiches</a></li>
+		</ul>
+	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+	</li>
+	<li><a href="starters.php">STARTERS<!--[if IE 7]><!--></a><!--<![endif]-->
+	<!--[if lte IE 6]><table><tr><td><![endif]-->
+		<ul>
+		<li><a href="starters.php?type=1">Starters</a></li>
+		<li><a href="starters.php?type=2">Soups</a></li>
+		<li><a href="starters.php?type=14">Salads</a></li>
+		</ul>
+	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+	</li>
+	<li><a href="sides.php">SIDES<!--[if IE 7]><!--></a><!--<![endif]-->
+	<!--[if lte IE 6]><table><tr><td><![endif]-->
+	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+	</li>
+	<li><a href="desserts.php">DESSERTS<!--[if IE 7]><!--></a><!--<![endif]-->
+	<!--[if lte IE 6]><table><tr><td><![endif]-->
+	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+	</li>
+	<li><a href="drinks.php">DRINKS<!--[if IE 7]><!--></a><!--<![endif]-->
+	<!--[if lte IE 6]><table><tr><td><![endif]-->
+		<ul>
+		<li><a href="drinks.php?type=13">Beverages</a></li>
+		<li><a href="drinks.php?type=16">Mixed Drinks</a></li>
+		<li><a href="drinks.php?type=17">Beers</a></li>
+		<li><a href="drinks.php?type=18">Wines</a></li>
+		</ul>
+	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+	</li>
+
+	</ul>
+
+</div><!-- end dd_menu -->
